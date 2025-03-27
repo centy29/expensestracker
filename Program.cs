4d867543eabd467d4105dbcb55cf950cfd61a057
centy29 <vincesmoke666@gmail.com>
2025-03-28 00:29:49 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,176 +1,131 @@
 ﻿using System;
 using System.Transactions;
+using System.Collections.Generic;
+using System.ComponentModel.Design;// this is needed to recognize the list
+using expenses_businesslogic;
 
 namespace expenses_tracker
 {
     internal class Program
     {
+
+        static string[] options = { "1. ADD EXPENSES", "2. PAYMENT METHOD", "3. HISTORY", "4.EXIT " }; // options for the user
+      
         static void Main(string[] args) //main method nga
         {
 
             Console.WriteLine("WELCOME TO EXPENSES TRACKER");
 
-            //variables tsaka values tapos pin login
+            string pin = "centy"; //variable name tsaka pin and username
+            string userpin;
+            do
+            {
+                Console.WriteLine("ENTER PIN: ");
+                userpin = Console.ReadLine();
 
-            string pin = "centy";
-            string username;
+                if (userpin != pin)
+                {
+                    Console.WriteLine("--------------------------");
+                    Console.WriteLine("ERROR!, ENTER THE CORRCT PIN!");
+                }
+                
 
-            //looping tsaka creating input
+            } while (userpin != pin);//LOOP UNTIL THE USER ENTERS THE CORRECT PIN
+
+            DisplayOptions();
+            int userinput = GetUserInput();//this is for the user to input their selected option
 
             do
             {
-                Console.Write("ENTER YOUR NAME: ");
-                username = Console.ReadLine();
-                if (username != pin)
+                DisplayOptions();// to display again after executing or finishing an option
+                userinput = GetUserInput();
+                switch (userinput)
                 {
-
-                    Console.WriteLine("--------------");
-                    Console.WriteLine("LOGGED IN...");
-
-                }
-                else {
-                    Console.WriteLine("-----------------");
-                    Console.WriteLine("WRONG PIN, ENTER THE CORRECT PIN!!!");
-                }
-
-            }
-            while (username == pin);
-
-                
-
-                    string[] options = { "1. ADD EXPENSES", "2. PAYMENT METHOD", "3. PAYMENT HISTORY", "4.EXIT " };
-                    Console.WriteLine("--------------");
-                    Console.WriteLine("SELECT AN OPTION");
-                    Console.WriteLine("--------------");
-
-
-                    foreach (var option in options)
-                    {
-                        Console.WriteLine(option);
-                    }
-                    Console.WriteLine("ENTER AN OPTION");
-                    Console.WriteLine("---------------");
-
-                    int useroption = Convert.ToInt16(Console.ReadLine());
-
-                   while (useroption != 4)
-                    {
-
-                        switch (useroption)
-                        {
-
-                            case 1:
-
-                                {
-                                    AddDetails();
-                                }
-                                break;
-
-                            case 2:
-                                Console.WriteLine(PaymentMethod);
-                                Console.WriteLine("");
-                                break;
-
-                            case 3:
-                                Console.WriteLine(PaymentHistory());
-                                Console.WriteLine(" ");
-                                break;
-
-                            case 4:
-                                Console.WriteLine("THANKS FOR USING EXPENSES TRACKER");
-
-                                break;
-
-                            default:
-                                Console.WriteLine("INVALID CHOICE");
-                                break;
-                        }
-                    }
-
-                    foreach (var option in options)
-                    {
-                        Console.WriteLine(option);
-                    }
-                    Console.WriteLine("ENTER AN OPTION");
-                    Console.WriteLine("---------------");
-
-                    useroption = Convert.ToInt16(Console.ReadLine());
-
-                static void AddDetails() //method for adding details
-                {
-                    while (true)
-                    {
-
-                        Console.WriteLine("ADD THE EXPENSES DETAILS");
-
-                        Console.WriteLine("------------------------");
-                        Console.WriteLine("ENTER THE BRAND");
-                        string brand = Console.ReadLine();
-
-                        Console.WriteLine("------------------------");
-                        Console.WriteLine("ENTER THE ADDRESS");
-                        string address = Console.ReadLine();
-
-                        Console.WriteLine("------------------------");
-                        Console.WriteLine("ENTER TIN ID NUMBER");
-                        int tin = Convert.ToInt16(Console.ReadLine());
-
-                        Console.WriteLine("------------------------");
-                        Console.WriteLine("ENTER INVOICE NUMBER");
-                        int invoice = Convert.ToInt16(Console.ReadLine());
-
-                        Console.WriteLine("------------------------");
-                        Console.WriteLine("ENTER THE AMOUNT SPENT");
-                        double amount = Convert.ToInt16(Console.ReadLine());
-
-
-
-                    }
+                    case 1:
+                        AddExpenses();//TO DISPLAY THE ADD EXPENSE METHOD
+                        break;
+                    case 2:
+                       expenselogic. paymentmethod();//TO DISPLAY THE PAYMENT METHOD
+                        break;
+                    case 3:
+                        history();//TO DISPLAY THE HISTORY INPUT 
+                        break;
+                    case 4:
+                        Console.WriteLine("---------------------------------");
+                        Console.WriteLine("THANKS FOR USING EXPENSES TRACKER");
+                        break;
+                    default:
+                        Console.WriteLine("INVALID INPUT PLEASE CHOOSE NUMBER 1 TO 4");//TO REMIND THE USER TO CHOOSE ONLY 1 TO 4 IN OPTIONS
+                        break;
 
                 }
-                static string PaymentMethod() //methods to remove the money input
-                {
-                    Console.WriteLine("WHAT IS YOUR PAYMENT METHOD?(CASH OR CREDIT?)");
-                    string pay = Console.ReadLine();
-
-                    if (pay == "CASH")
-                    {
-                        Console.WriteLine("ENTER AMOUNT TO PAY...");
-                        double payable = Convert.ToInt16(Console.ReadLine());
-
-                    }
-
-                    return "";
-                }
-                static string PaymentHistory()
-                {
-                Console.WriteLine("-----------------");
-                Console.WriteLine("BRAND:" );
-
-                Console.WriteLine("-----------------");
-                Console.WriteLine("ADDRESS:");
-
-                Console.WriteLine("-----------------");
-                Console.WriteLine("TIN:");
-
-                Console.WriteLine("-----------------");
-                Console.WriteLine("INVOICE:");
-
-                 Console.WriteLine("----------------");
-                Console.WriteLine("AMOUNT: ");
-                return " THIS IS THE HISTORY OF YOU LISTS";
-
-                }
-
-
-
-
-
-
-
-
-            
+            } while (userinput != 4);
         }
+
+        static void DisplayOptions()//easiest method para sa looping ni mam
+        {
+            Console.WriteLine("----------------========");
+            Console.WriteLine("PICK AN OPTION: ");
+
+            foreach (var option in options) //this is used for displaying the list of options
+            {
+                Console.WriteLine(option);
+            }
+        }//UI LOGIC
+        static int GetUserInput()
+        {
+            Console.WriteLine("------------------------");
+            int userinput = Convert.ToInt16(Console.ReadLine());
+            return userinput;// return or loop to make another input
+        }//UI LOGIC
+
+        static void AddExpenses() {
+
+            Console.WriteLine("ADD THE EXPENSES DETAILS");
+
+            Console.WriteLine("------------------------");
+            Console.WriteLine("ENTER THE BRAND");
+            string brand = Console.ReadLine();
+
+            Console.WriteLine("------------------------");
+            Console.WriteLine("ENTER THE ADDRESS");
+            string address = Console.ReadLine();
+
+            Console.WriteLine("------------------------");
+            Console.WriteLine("ENTER TIN ID NUMBER");
+            int tin = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("------------------------");
+            Console.WriteLine("ENTER INVOICE NUMBER");
+            int invoice = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("------------------------");
+            Console.WriteLine("ENTER THE AMOUNT SPENT");
+            double amount = Convert.ToInt16(Console.ReadLine());
+
+            expenselogic. expenses.Add((brand, address, tin, invoice, amount));// a method for adding the input from here to store in the list
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("THE EXPENSES ADDED SUCCESSFULLY");
+            Console.WriteLine("-------------------------------");
+
+            //MORE UPDATE: MAKE IT SAVED TO A DATABASE FOR EASY STORING
+        }// PROCESSING LOGIC BL/DL
+
+      
+        static void history() {
+            foreach (var expense in expenselogic. expenses)//USED TO DISPLAY THE LIST OF EXPENSES NA NAINPUT OR NA ADD
+            {
+                Console.WriteLine("-----------------------");
+                Console.WriteLine("brand: " + expense.brand);
+                Console.WriteLine("adreess: " + expense.address);
+                Console.WriteLine("tin: " + expense.tin);
+                Console.WriteLine("invoice: " + expense.invoice);
+                Console.WriteLine("amount: " + expense.amount);
+                Console.WriteLine("-----------------------");
+            }
+        }// PROCESSING LOGIC BL/DL
+        //MORE UPDATE: CAN CHOOSE THE ITEM THEY WANT A HISTORY OR SHOWS ALL THE ITEMS STORED IN THE LIST
+
 
     } 
 }
